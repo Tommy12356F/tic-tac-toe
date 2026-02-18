e1f5533534316800a06dce46f4ed715f031e0159
Vedant <vedant.vidhani@gmail.com>
2026-02-18 23:40:04 +0530

    defined basic js structure

---
+++ script.js
@@ -0,0 +1,23 @@
+const Gameboard = (function() {
+
+    // private stuff here
+    const board=["", "", "", "", "", "", "", "", ""]
+    const placeMark = (index, marker) => {
+
+    };
+    const reset = ()=>{
+
+    };
+    const getBoard = ()=>{
+        return board;
+
+    };
+
+    return {
+        // public stuff here
+        getBoard, placeMark, reset 
+    };
+    
+    
+
+})();
